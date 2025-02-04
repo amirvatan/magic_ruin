98147adef29894908e643fa8b6ab1665da220afa
amirvatan <amirvatan0011@gmail.com>
2025-02-04 22:29:59 +0330

    fix dynamic arrays to work for every type of data

--- engine/basic utilities/dynamic_array.c
+++ engine/basic utilities/dynamic_array.c
@@ -1,24 +1,11 @@
 #include "dynamic_array.h"
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
 
-size_t array_size(d_DataType type) {
-  size_t d_size;
-  switch (type) {
-  case D_INT:
-    d_size = sizeof(int);
-    break;
-  case D_FLOAT:
-    d_size = sizeof(float);
-    break;
-  default:
-    d_size = 0;
-  }
-  return d_size;
-}
-
-d_array init_array(size_t size, d_DataType type) {
-  size_t d_size = array_size(type);
+d_array init_array(size_t size, size_t d_size) {
   d_array array = {
-      .type = type,
+      .d_size = d_size,
       .size = size,
       .index = 0,
       .data = malloc(size * d_size),
@@ -31,19 +18,14 @@ void push_array(d_array *array, void *value) {
   size_t index = array->index;
   if (size == index) {
     array->size = size * 2;
-    array->data = realloc(array->data, array->size * array_size(array->type));
+    array->data = realloc(array->data, array->size * array->d_size);
     if (array->data == NULL) {
       printf("Add new element to array : Array realloc failed.\n");
       return;
     }
   }
-  if (array->type == D_INT) {
-    int *intptr = (int *)value;
-    ((int *)array->data)[index] = *intptr;
-  } else {
-    float *fltptr = (float *)value;
-    ((float *)array->data)[index] = *fltptr;
-  }
+  memcpy((unsigned int *)array->data + index * array->d_size, value,
+         array->d_size);
   array->index = ++index;
 }
 void insert_array(d_array *array, void *value, size_t idx) {
@@ -54,51 +36,29 @@ void insert_array(d_array *array, void *value, size_t idx) {
     return;
   }
   if (size == idx) {
-    array->size = size * 2;
-    array->data = realloc(array->data, array->size * array_size(array->type));
-    if (array->data == NULL) {
-      printf("Add new element to array : Array realloc failed.\n");
-      return;
-    }
+    push_array(array, value);
+    return;
   }
-  if (array->type == D_INT) {
-    int *intptr = (int *)value;
-    ((int *)array->data)[idx] = *intptr;
-  } else {
-    float *fltptr = (float *)value;
-    ((float *)array->data)[idx] = *fltptr;
-  }
-  if (idx == index) {
-
-    array->index = ++index;
-  }
+  memcpy((unsigned int *)array->data + idx * array->d_size, value,
+         array->d_size);
 }
 
 d_array copy_array(d_array *array) {
-  size_t d_size = array_size(array->size);
+  d_array cp_array = init_array(array->size, array->d_size);
 
-  d_array cp_array = init_array(array->size, array->type);
-
-  memcpy(cp_array.data, array->data, array->size * d_size);
+  memcpy((unsigned int *)cp_array.data, (unsigned int *)array->data,
+         array->size * array->d_size);
 
   return cp_array;
 }
 
 void *head_array(d_array *array) {
   int index = (*array).index;
-  if (array->type == D_INT) {
-    return &((int *)array->data)[index - 1];
-  } else {
-    return &((float *)array->data)[index - 1];
-  }
+  return (unsigned int *)array->data + (array->index - 1) * array->d_size;
 }
 
 void *pop_array(d_array *array) {
   int index = (*array).index;
   array->index = index - 1;
-  if (array->type == D_INT) {
-    return &((int *)array->data)[index - 1];
-  } else {
-    return &((float *)array->data)[index - 1];
-  }
+  return (unsigned int *)array->data + index * array->d_size;
 }
--- engine/basic utilities/dynamic_array.h
+++ engine/basic utilities/dynamic_array.h
@@ -2,22 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef enum {
-  D_INT,
-  D_FLOAT,
-} d_DataType;
-
 typedef struct {
-  d_DataType type;
+  size_t d_size;
   size_t size;
   size_t index;
   void *data;
 } d_array;
 
-d_array init_array(size_t size,d_DataType type);
+d_array init_array(size_t size,size_t d_size);
 void push_array(d_array * array,void * value);
 d_array copy_array(d_array * array);
-size_t array_size(d_DataType type);
 void * pop_array(d_array * array);
 void * head_array(d_array * array);
 void insert_array(d_array * array,void * value,size_t idx);
